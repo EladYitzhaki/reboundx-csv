bc163e380db5c6cdae92ff0855faefab334d9388
EladYitzhaki <120108176+EladYitzhaki@users.noreply.github.com>
2023-06-27 15:39:13 +0300

    Add files via upload

--- analysis1csv.py
+++ analysis1csv.py
@@ -12,6 +12,7 @@ line_number = int(sys.argv[2])
 parameter = sys.argv[3]
 full_filename = sys.argv[1] # bin file
 filename0 = os.path.splitext(full_filename)[0]
+file_number = filename0.split("_")[-1]
 # print(filename0)
 filename1 = "{0}.cart.csv".format(filename0)
 filename2 = "{0}.orb.csv".format(filename0)
@@ -64,46 +65,46 @@ with open(filename5, 'a') as file:
                     writer.writerow(data_point)
             # planet += 1
         del df
-    if parameter == 'a':
-        df = pd.read_csv(filename2)
-        planet = 0
-        for element in range(len(df.columns)):
-            num = (element - 1)
-            if num % 6 == 0:
-                writer = csv.writer(file)
-                data_point = [df.iat[line_number, element]]
-                if np.isnan(data_point):
-                    continue
-                else:
-                    writer.writerow(data_point)
-        del df
-    if parameter == 'e':
-        df = pd.read_csv(filename2)
-        planet = 0
-        for element in range(len(df.columns)):
-            num = (element - 2)
-            if num % 6 == 0:
-                writer = csv.writer(file)
-                data_point = [df.iat[line_number, element]]
-                writer.writerow(data_point)
-                if np.isnan(data_point):
-                    continue
-                else:
-                    writer.writerow(data_point)
-        del df
-    if parameter == 'inc':
-        df = pd.read_csv(filename2)
-
-        for element in range(len(df.columns)):
-            num = (element - 3)
-            if num % 6 == 0:
-                writer = csv.writer(file)
-                data_point = [df.iat[line_number, element]]
-                if np.isnan(data_point):
-                    continue
-                else:
-                    writer.writerow(data_point)
-        del df
+    # if parameter == 'a':
+    #     df = pd.read_csv(filename2)
+    #     planet = 0
+    #     for element in range(len(df.columns)):
+    #         num = (element - 1)
+    #         if num % 6 == 0:
+    #             writer = csv.writer(file)
+    #             data_point = [df.iat[line_number, element]]
+    #             if np.isnan(data_point):
+    #                 continue
+    #             else:
+    #                 writer.writerow(data_point)
+    #     del df
+    # if parameter == 'e':
+    #     df = pd.read_csv(filename2)
+    #     planet = 0
+    #     for element in range(len(df.columns)):
+    #         num = (element - 2)
+    #         if num % 6 == 0:
+    #             writer = csv.writer(file)
+    #             data_point = [df.iat[line_number, element]]
+    #             writer.writerow(data_point)
+    #             if np.isnan(data_point):
+    #                 continue
+    #             else:
+    #                 writer.writerow(data_point)
+    #     del df
+    # if parameter == 'inc':
+    #     df = pd.read_csv(filename2)
+    #
+    #     for element in range(len(df.columns)):
+    #         num = (element - 3)
+    #         if num % 6 == 0:
+    #             writer = csv.writer(file)
+    #             data_point = [df.iat[line_number, element]]
+    #             if np.isnan(data_point):
+    #                 continue
+    #             else:
+    #                 writer.writerow(data_point)
+    #     del df
 
     if parameter == 'aei':
         # if line_number == '-1':
@@ -115,24 +116,74 @@ with open(filename5, 'a') as file:
             except OSError:
                 f.seek(0)
             last_line = f.readline().decode().split(',')
+            last_line[-1]=last_line[-1].split("\r")[0]
             time = last_line[0]
-            planet = 0
-            for element in range(len(last_line)):
-                data_row = [time]
-                num = (element - 1)
-                if num % 6 == 0:
-                    data_point1 = last_line[element]
-                    data_row.append(data_point1)
-                    data_point2 = last_line[element+1]
-                    data_row.append(data_point2)
-                    data_point3 = last_line[element+2]
-                    data_row.append(data_point3)
-                    periastron = float(data_point1)*(1-float(data_point2))
-                    data_row.append(periastron)
-                    planet = planet+ 1
-                    data_row.append(planet)
-                    writer = csv.writer(file)
-                    writer.writerow(data_row)
+            num_of_planets= int(np.floor(len(last_line)-1)/6)
+            print("num_of_planets: ",num_of_planets)
+            N = 0
+            for planet_index in range(num_of_planets):
+                if float(last_line[planet_index * 6 + 1]) > 0:
+                    N = N + 1
+            planets=[[]] *N
+            for planet_index in range(num_of_planets):
+                # print(planet_index)
+                if float(last_line[planet_index*6+1])>0:
+                    planets[planet_index]=last_line[planet_index*6+1:planet_index*6+1+6]
+                    # N=N+1
+                    # print(planets)
+            for planet in planets:
+                print(planet[0])
+            # sorting the planets array by the semi major axis
+            planets= sorted(planets, key=lambda planet: planet[0]) #reverse=True
+            # print(planets)
+
+            print('N' , N)
+            for planet in planets:
+                print(planet[0])
+
+            if N !=0:
+                new_last_line= [planets[0:N]]
+                new_last_line= sum(new_last_line,[])
+
+            # print(new_last_line)
+            #flat_list = list(np.concatenate(new_last_line).flat)
+            last_line = sum(new_last_line, [time])
+            # print(last_line)
+
+            filename6 = "output.{1}p.time{0}.V2.csv".format(line_number * 100,N)
+
+            # planet = 0
+            # # print(len(last_line))
+            # if len(last_line) == 1+6:
+            #     filename6 = "output.1p.time{0}.csv".format(line_number*100)
+            # elif len(last_line) == 1+2*6:
+            #     filename6 = "output.2p.time{0}.csv".format(line_number*100)
+            # elif len(last_line) == 1+3*6:
+            #     filename6 = "output.3p.time{0}.csv".format(line_number*100)
+            # else:
+            #     filename6 = "output.more_p.time{0}.csv".format(line_number * 100)
+            with open(filename6, 'a') as file2:
+                writer = csv.writer(file2)
+                writer.writerow(last_line)
+
+
+            # for element in range(len(last_line)):
+            #     data_row = [time]
+            #     num = (element - 1)
+            #     if num % 6 == 0:
+            #         data_point1 = last_line[element]
+            #         data_row.append(data_point1)
+            #         data_point2 = last_line[element+1]
+            #         data_row.append(data_point2)
+            #         data_point3 = last_line[element+2]
+            #         data_row.append(data_point3)
+            #         periastron = float(data_point1)*(1-float(data_point2))
+            #         data_row.append(periastron)
+            #         planet = planet+ 1
+            #         data_row.append(planet)
+            #         writer = csv.writer(file)
+            #         writer.writerow(data_row)
+
 
         # ChunkSize = 10000
         # for chunk in pd.read_csv(filename2, ChunkSize= ChunkSize,skiprows= line_number, keep_default_na= False):
@@ -161,9 +212,9 @@ with open(filename5, 'a') as file:
         #
         #     del df
 
-    if parameter == 'coll':
-        df = pd.read_csv(filename3)
-        del df
-    if parameter == 'eject':
-        df = pd.read_csv(filename4)
-        del df
+    # if parameter == 'coll':
+    #     df = pd.read_csv(filename3)
+    #     del df
+    # if parameter == 'eject':
+    #     df = pd.read_csv(filename4)
+    #     del df
